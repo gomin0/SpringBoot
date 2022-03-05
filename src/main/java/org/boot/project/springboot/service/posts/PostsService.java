70e3e435ab4578cbb33aa6456c9ac771aa3348d1
gomin0 <gomin0981113@naver.com>
2022-03-05 14:33:16 +0900

    2022-03-05

--- src/main/java/org/boot/project/springboot/service/posts/PostsService.java
+++ src/main/java/org/boot/project/springboot/service/posts/PostsService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @RequiredArgsConstructor
 @Service
 public class PostsService {
@@ -45,5 +44,12 @@ public class PostsService {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new) // posts -> new PostsListResponseDto(post)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);  // JpaRepository의 delete 메소드
     }
 }
--- src/main/java/org/boot/project/springboot/web/IndexController.java
+++ src/main/java/org/boot/project/springboot/web/IndexController.java
@@ -2,9 +2,11 @@ package org.boot.project.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.boot.project.springboot.service.posts.PostsService;
+import org.boot.project.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,5 +25,11 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
 
+        return "posts-update";
+    }
 }
